bce3abe7bbd8c26390e9d432270b6a4bf8b3d7cf
Sarah Dayan <dayan.sarah@gmail.com>
2018-09-24 20:06:34 +0200

    Remove test directive from master

--- src/directives/test.js
+++
@@ -1,7 +0,0 @@
-export default (el, binding) => {
-  if (process.env.NODE_ENV === 'test') {
-    Object.keys(binding.value).forEach(value => {
-      el.setAttribute(`data-test-${value}`, binding.value[value])
-    })
-  }
-}
